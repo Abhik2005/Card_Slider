75a9072719a8897d240a574e363fec8804fc3008
Abhik2005 <abhikmondal532@gmail.com>
2024-03-14 01:40:55 +0530

    add songs

--- songs.js
+++ songs.js
@@ -10,22 +10,29 @@ const sliderArr = [
       id: 1,
       imgUrl:
         "https://images.unsplash.com/photo-1707343843437-caacff5cfa74?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHw2fHx8ZW58MHx8fHx8",
-      title: "Desert",
+      title: "Mujhe pine Do",
       musicUrl: "./songs/m1.m4a"
     },
     {
       id: 2,
       imgUrl:
         "https://i.ytimg.com/vi/YxWlaYCA8MU/mqdefault.jpg",
-      title: "Heal",
-      musicUrl: "./songs/m1.m4a"
+      title: "Jhoome Jo Pathaan Song | Shah Rukh Khan, Deepika | Vishal & Sheykhar, Arijit Singh, Sukriti, Kumaar",
+      musicUrl: "./songs/Jhoome_Jo_Pathaan.mp3"
     },
     {
       id: 3,
       imgUrl:
-        "https://plus.unsplash.com/premium_photo-1710118735364-c04ad6961401?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxN3x8fGVufDB8fHx8fA%3D%3D",
-      title: "Food",
-      musicUrl: "m1.m4a"
+        "https://i.ytimg.com/vi/BddP6PYo2gs/mqdefault.jpg",
+      title: "Kesariya - Brahmāstra | Ranbir Kapoor | Alia Bhatt | Pritam | Arijit Singh | Amitabh Bhattacharya",
+      musicUrl: "./songs/Kesariya.mp3"
+    },
+    {
+      id: 4,
+      imgUrl:
+        "https://i.ytimg.com/vi/k1K7F4Oz_oQ/mqdefault.jpg",
+      title: "Dil Ne Ye Kaha Hai Dil Se ((Jhankar)) 4K HD Video | Dhadkan | Akshay Kumar, Shilpa Shetty",
+      musicUrl: "./songs/Dil_Ne_Ye_Kaha_Hai.mp3"
     },
   ];
 
